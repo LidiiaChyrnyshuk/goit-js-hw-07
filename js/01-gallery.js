8e1b7ff45ebb0116dec74b23b43f4c1ba6e0b0a7
Lidiia Chyrnyshuk <l.mandych@gmail.com>
2023-03-03 09:13:13 +0200

    add task_2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -50,4 +50,4 @@ function handleImageClick(event) {
 			instance.close();
 		}
 	});
-}
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerEl = document.querySelector(".gallery");
+const cardsMurkup = createImageCardsMurkup(galleryItems);
+galleryContainerEl.insertAdjacentHTML("beforeend", cardsMurkup);
+
+// створюю розмітку
+function createImageCardsMurkup(galleryItems) {
+	return galleryItems
+		.map(({ preview, original, description }) => {
+			return `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`;
+		})
+		.join("");
+}
+console.log(createImageCardsMurkup(galleryItems));
+
+const lightbox = new SimpleLightbox(".gallery a", {
+	captionPosition: "bottom",
+	captionDelay: 250,
+	captionsData: "alt",
+});
+
+lightbox.on("show.simplelightbox");
